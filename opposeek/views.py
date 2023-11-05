9b0406c47346f5037ae40b80626300ec9e37cea1
Johnny <johnkcli@hotmail.com>
2023-11-05 20:23:54 +0000

    receives search

--- opposeek/views.py
+++ opposeek/views.py
@@ -1,5 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from opposeek.forms import SearchForm
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    search = request.GET.get("search")
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(f"/?search={form.cleaned_data['search']}")
+    elif search:
+        form = SearchForm(request.GET)
+    else:
+        form = SearchForm()
+
+    return render(request, "index.html", {"form": form})
